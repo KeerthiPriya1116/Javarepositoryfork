1ebc29bf225d983484839aaf568b73d9d1033f82
pkeer <pkeer@DESKTOP-BUK2US3>
2024-07-27 10:48:18 +0530

    added project

---
+++ project112/src/project112/Class1.java
@@ -0,0 +1,12 @@
+package project112;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Class1");
+          System.out.println("Class2");
+          System.out.println("Class3");
+	}
+
+}
