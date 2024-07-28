c8f31981615356e90d6e878f2bfc18f9cb1a6cc6
LOKI <LOKI@DEVI>
2024-07-28 20:46:10 +0530

    updated

--- project112/src/project112/Class1.java
+++ project112/src/project112/Class1.java
@@ -4,7 +4,7 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          System.out.println("Class9");
+          System.out.println("Class10");
           System.out.println("Class5");
           System.out.println("Class6");
 	}
