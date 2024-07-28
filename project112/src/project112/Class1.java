849f51ec3fffeeca1f1da862bffb89413062b8a2
pkeer <pkeer@DESKTOP-BUK2US3>
2024-07-28 20:21:03 +0530

    updated class1

--- project112/src/project112/Class1.java
+++ project112/src/project112/Class1.java
@@ -4,9 +4,9 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          System.out.println("Class1");
-          System.out.println("Class2");
-          System.out.println("Class3");
+          System.out.println("Class4");
+          System.out.println("Class5");
+          System.out.println("Class6");
 	}
 
 }
